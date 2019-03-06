e755ee7220034e7244c9b79c328946940c427035
admin <admin@admindeMacBook-Pro.local>
2019-03-06 14:58:24 +0800

    dist --update

--- js/vendor~493df0b3.b38d84fb144ca8374d41.js
+++ js/vendor~493df0b3.b38d84fb144ca8374d41.js
@@ -1,4 +1,4 @@
-(window.webpackJsonp=window.webpackJsonp||[]).push([[4],{"../node_modules/object-assign/index.js":function(e,t,n){"use strict";
+(window.webpackJsonp=window.webpackJsonp||[]).push([[5],{"../node_modules/object-assign/index.js":function(e,t,n){"use strict";
 /*
 object-assign
 (c) Sindre Sorhus
