6a5598a2d86d2635450445342c16255700cef260
admin <admin@admindeMacBook-Pro.local>
2019-03-13 16:13:28 +0800

    mirror --update

---
+++ src/index.js
@@ -0,0 +1,21 @@
+export { default as KoBar } from './packages/bar';
+export { default as KoCandle } from './packages/candle';
+export { default as KoFunnel } from './packages/funnel';
+export { default as KoGuage } from './packages/gauge';
+
+export { default as KoHeatmap } from './packages/heatmap';
+export { default as KoHistogram } from './packages/histogram';
+export { default as KoLine } from './packages/line';
+export { default as KoLiquidfill } from './packages/liquidfill';
+
+export { default as KoMap } from './packages/map';
+export { default as KoPie } from './packages/pie';
+export { default as KoRadar } from './packages/radar';
+export { default as KoRing } from './packages/ring';
+
+export { default as KoSankey } from './packages/sankey';
+export { default as KoScatter } from './packages/scatter';
+export { default as KoTree } from './packages/tree';
+export { default as KoWaterfall } from './packages/waterfall';
+
+export { default as KoWordcloud } from './packages/wordcloud';
